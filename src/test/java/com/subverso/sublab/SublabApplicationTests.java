8413d0228bfe8209284c8839bf5f5f04f79d473c
jade <jadenogueira8881@gmail.com>
2025-10-31 10:10:33 -0300

    first commit

---
+++ src/test/java/com/subverso/sublab/SublabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subverso.sublab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SublabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
