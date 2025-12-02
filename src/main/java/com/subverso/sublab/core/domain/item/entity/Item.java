e8b6d7033d75a73af85bb148ac9521f6a8e72fce
jade <jadenogueira8881@gmail.com>
2025-12-02 16:19:08 -0300

    refactor: remove unused import for JoinColumn in Item entity

--- src/main/java/com/subverso/sublab/core/domain/item/entity/Item.java
+++ src/main/java/com/subverso/sublab/core/domain/item/entity/Item.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
